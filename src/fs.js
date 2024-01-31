3085a22b47554636a0f6b2f5b8c435ab811cb02b
mina <minakarimi1@yahoo.com>
2024-02-01 00:01:30 +0330

    added Math in generateCaptcha

--- src/fs.js
+++ src/fs.js
@@ -2,6 +2,6 @@ export const generateCaptcha =()=>{
     const num1 = Math.floor(Math.random() * 10);
     const num2 = Math.floor(Math.random() * 10);
     const operation = ['+', '-', '*'][Math.floor(Math.random() * 3)];
-
+    const result = eval(`${num1} ${operation} ${num2}`);
 
 }
