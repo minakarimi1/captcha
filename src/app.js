3321d5a12b25f021b8cb4202cd655cde6d6ea7d4
Mohammad hadi <m6336.programer@gmail.com>
2024-02-17 16:22:05 +0330

    reapaire

--- src/app.js
+++ src/app.js
@@ -1,39 +1,52 @@
-import 'dotenv'
-import {generateCaptcha} from './fs.js'
-import express, { urlencoded } from "express";
+import 'dotenv/config';
+import express from 'express';
+import { generateCaptcha } from './fs.js';
 
-const PORT=process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Middleware
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/',(req,res)=>{
-    const { captcha, result } = generateCaptcha();
-    res.render('index',{result,captcha})
-
-})
-
-app.get('/captchaReq', (req, res) => {
-    const { captcha, result } = generateCaptcha();
-    res.send({captcha:captcha,result:result})
-});
-
-app.post('/submit', (req, res) => {
-    const userAnswer = parseInt(req.body.answer);
-    const correctAnswer = parseInt(req.body.result);
-    if (userAnswer === correctAnswer) {
-        res.status(200).send({message:'جواب صحیح'});
-    } else {
-    res.status(404).send({message:'جواب اشتباه'});
+// Routes
+app.get('/', (req, res) => {
+    try {
+        const { captcha, result } = generateCaptcha();
+        res.render('index', { result, captcha });
+    } catch (error) {
+        console.error('Error generating captcha:', error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
-app.listen((PORT,()=>{
-    console.log(`server is running on port: ${PORT}`);
-}))
+app.get('/captchaReq', (req, res) => {
+    try {
+        const { captcha, result } = generateCaptcha();
+        res.send({ captcha, result });
+    } catch (error) {
+        console.error('Error generating captcha:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
+app.post('/submit', (req, res) => {
+    try {
+        const userAnswer = parseInt(req.body.answer);
+        const correctAnswer = parseInt(req.body.result);
+        if (userAnswer === correctAnswer) {
+            res.status(200).send({ message: 'جواب صحیح' });
+        } else {
+            res.status(404).send({ message: 'جواب اشتباه' });
+        }
+    } catch (error) {
+        console.error('Error processing submission:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
